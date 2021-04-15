eb46ba9061aafe041552763b19a2fbc788ea09ee
Nassim Essaidi <contact@essaidi.me>
2021-04-15 18:05:20 +0000

    refresh env

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <div className="overflow-hidden bg-gray-50 flex h-screen">
+        {/**center vertical & horizontal */}
         <div className="m-auto">
           <Main />
         </div>
